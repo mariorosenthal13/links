5ae5b70692f23aaadb72a265bea4e4e11a18b73e
mariorosenthal13 <mario.f.rosenthal@gmail.com>
2017-06-30 01:15:51 -0400

    links can be added

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     $links = \App\Link::all();
@@ -19,3 +20,26 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/submit', function(){
+    return view('submit');
+});
+
+Route::post('/submit', function(Request $request){
+    $validator = Validator::make( $request->all() , [
+        'title' => 'required | max:255',
+        'url' => 'required | max:255',
+        'description' => 'required | max:255',
+    ]);
+    if ($validator->fails()){
+        return back()
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $link = new App\Link;
+    $link->title = $request->title;
+    $link->url = $request->url;
+    $link->description = $request->description;
+    $link->save();
+    return redirect ('/');
+});
